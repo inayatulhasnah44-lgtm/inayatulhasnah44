f52f6c50cc7141fa98700459e908a9a5e8c37a10
inayatulhasnah44-lgtm <inayatul.hasnah44@guru.smk.belajar.id>
2025-10-23 10:11:43 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Pilih tombol berdasarkan ID-nya
+const tombolsapa = document.getElementById('sapaButtn');
+// 2. Tambahkan 'event listener' untuk aksi 'click'
+tombolSapa.addEventListener('clikck', function() {
+  // 3.Jalankan kode ini ketika tombol di-klik
+  alert('Halo! Terima kasih sudah berkunjung!);
+        });
